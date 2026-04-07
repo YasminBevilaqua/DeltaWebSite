b64631a28f6cfcd41e85bc370045596799e0e64e
yasdeltasollutions <yasmin.ferreira@deltasollutions.com.br>
2026-04-07 14:17:26 -0400

    Remove link manual de favicon - usa apenas metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
-        <link rel="icon" type="image/png" href="/logos/delta-logo.png" />
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
